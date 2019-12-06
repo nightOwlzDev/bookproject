8f185205d87862b90f52d768722d3f756d74d0b7
Night <chanwit.sua@acacia-it.co.th>
2019-12-06 16:35:21 +0700

    fix bug

--- src/server.js
+++ src/server.js
@@ -14,37 +14,7 @@ app.use(cors({ origin:'http://localhost:3000'}));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-<<<<<<< HEAD
-mongoose.connect("mongodb://localhost:27017/luvreadproject", { useNewUrlParser: true });
-=======
 mongoose.connect("mongodb://localhost:27017/luvreadproject", { useNewUrlParser: true })
-
-
-app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-})
-
-
-app.post('/products', async (req, res) => {
-    const payload = req.body
-    const product = new Product(payload)
-    await product.save()
-    res.status(201).end()
-})
-
-// app.put('/products', async (req, res) => {
-//     const payload = req.body
-//     const product = new Product(payload)
-//     await product.save()
-//     res.status(201).end()
-// })
-
-app.delete('/products/:id', async (req, res) => {
-    const { id } = req.params
-    await Product.findByIdAndDelete(id)
-    res.status(204).end()
->>>>>>> 62fa308f0c63d561deadbaa44176daea71f41e15
 
 // ดึงข้อมูล products ทั้งหมด
 app.get("/products", productmodule.getProduct);
